cc2b37bf851ad826a57b9fd6f4f3a579f932a582
Botspot <j.allen.marchand@outlook.com>
2020-10-18 08:29:47 -0500

    rm popup if not updatable

--- onstartup.sh
+++ onstartup.sh
@@ -17,13 +17,15 @@ while true;do
   
   LIST="$(apt list --upgradable 2>/dev/null | cut -d/ -f 1 | tail -n +2)"
   
-  echo -e "$LIST" | yad --center --title='Update Buddy' --width=310 --height=300 --no-headers \
-    --list --separator='\n' --window-icon="${DIRECTORY}/logo.png" \
-    --text='These packages can be upgraded:' \
-    --column=Package \
-    --button='Cancel'!"${DIRECTORY}/icons/exit.png"!:1 \
-    --button='Update now'!"${DIRECTORY}/icons/download.png":0 \
-    2>/dev/null || update=0
+  if [ $update == 1 ];then
+    echo -e "$LIST" | yad --center --title='Update Buddy' --width=310 --height=300 --no-headers \
+      --list --separator='\n' --window-icon="${DIRECTORY}/logo.png" \
+      --text='These packages can be upgraded:' \
+      --column=Package \
+      --button='Cancel'!"${DIRECTORY}/icons/exit.png"!:1 \
+      --button='Update now'!"${DIRECTORY}/icons/download.png":0 \
+      2>/dev/null || update=0
+  fi
   
   if [ $update == 1 ];then
     lxterminal --title='Upgrading packages' -e 'sudo apt -y full-upgrade;echo "Closing in 10 seconds.";sleep 10'
