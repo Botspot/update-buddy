6f85cc677ae0730a836c035f7d512f8c6748e749
Botspot <j.allen.marchand@outlook.com>
2020-10-17 21:18:56 -0500

    now checks every 6 hours

--- onstartup.sh
+++ onstartup.sh
@@ -2,24 +2,31 @@
 
 DIRECTORY="$(readlink -f "$(dirname "$0")")"
 
-output="$(sudo apt update 2>&1)"
-exitcode=$?
-
-#inform user packages are upgradeable
-if [ -z "$(echo "$output" | grep 'packages can be upgraded' )" ];then
-  exit 0
-fi
-#only continue script if upgrades available
-
-
-LIST="$(apt list --upgradable 2>/dev/null | cut -d/ -f 1 | tail -n +2)"
-
-echo -e "$LIST" | yad --center --title='Update Buddy' --width=310 --height=300 --no-headers \
-  --list --separator='\n' --window-icon="${DIRECTORY}/logo.png" \
-  --text='These packages can be upgraded:' \
-  --column=Package \
-  --button='Cancel'!"${DIRECTORY}/icons/exit.png"!:1 \
-  --button='Update now'!"${DIRECTORY}/icons/download.png":0 \
-  2>/dev/null || exit 0
+while true;do
+  update=1
   
-lxterminal --title='Upgrading packages' -e 'sudo apt -y full-upgrade;echo "Closing in 10 seconds.";sleep 10'
+  output="$(sudo apt update 2>&1)"
+  exitcode=$?
+  
+  #inform user packages are upgradeable
+  if [ -z "$(echo "$output" | grep 'packages can be upgraded' )" ];then
+    update=0
+  fi
+  #only continue script if upgrades available
+  
+  
+  LIST="$(apt list --upgradable 2>/dev/null | cut -d/ -f 1 | tail -n +2)"
+  
+  echo -e "$LIST" | yad --center --title='Update Buddy' --width=310 --height=300 --no-headers \
+    --list --separator='\n' --window-icon="${DIRECTORY}/logo.png" \
+    --text='These packages can be upgraded:' \
+    --column=Package \
+    --button='Cancel'!"${DIRECTORY}/icons/exit.png"!:1 \
+    --button='Update now'!"${DIRECTORY}/icons/download.png":0 \
+    2>/dev/null || update=0
+  
+  if [ $update == 0 ];then
+    lxterminal --title='Upgrading packages' -e 'sudo apt -y full-upgrade;echo "Closing in 10 seconds.";sleep 10'
+  fi
+  sleep 6h
+done
